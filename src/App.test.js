c49b0aec11bdb8feff3e5bdc134a6105f0c0f522
Daniel Calise <daniel.calise@aexp.com>
2020-07-30 16:55:02 -0400

    broken tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('broken test', () => {
   const div = document.createElement('div');
+  const correct = true;
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(correct).toBe(false);
 });
