e15e154111802f288c64d8f64c2f51911073a0bb
Dan Calise <me@dancali.se>
2020-08-04 08:52:02 -0400

    updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Grocery from "./components/Grocery";
+import Fridge from "./components/Fridge";
+import ShoppingCart from "./components/ShoppingCart";
 
-it('broken test', () => {
-  const div = document.createElement('div');
-  const correct = true;
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Grocery", () => {
+  test("should render an empty string if no props are passed", () => {
+    const { container } = render(<Grocery />);
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div />
+      </div>
+    `);
+  });
 
-  expect(correct).toBe(false);
+  test("should render a grocery component with the name passed via the name prop", () => {
+    const expectedName = "butter";
+    const { getByText } = render(<Grocery name={expectedName} />);
+    const groceryName = getByText(expectedName).textContent;
+    expect(groceryName).toBe(expectedName);
+  });
 });
+
+describe('Fridge', () => {
+  test('should render a fridge component that contains 3 groceries: milk, butter, eggs', () => {
+    const { getByText, container } = render(<Fridge />);
+    const milk = getByText(/milk/i).textContent;
+    const butter = getByText(/butter/i).textContent;
+    const eggs = getByText(/eggs/i).textContent;
+
+    expect(milk).toBe('milk');
+    expect(butter).toBe('butter');
+    expect(eggs).toBe('eggs');
+    expect(container.firstChild).toHaveClass('fridge')
+  });
+
+  test('should render a fridge component with a className of `fridge`', () => {
+    const { container } = render(<Fridge />);
+    expect(container.firstChild).toHaveClass('fridge');
+  });
+});
+
+describe('ShoppingCart', () => {
+  test('should render a ShoppingCart component with a className of `shopping-cart` that contains 3 groceries: milk, butter, eggs', () => {
+    const { getByText } = render(<ShoppingCart />);
+    const tomato = getByText(/tomato/i).textContent;
+    const banana = getByText(/banana/i).textContent;
+
+    expect(tomato).toBe('tomato');
+    expect(banana).toBe('banana');
+  });
+
+  test('should render a ShoppingCart component with a className of `shopping-cart` that contains 3 groceries: milk, butter, eggs', () => {
+    const { container } = render(<ShoppingCart />);
+    expect(container.firstChild).toHaveClass('shopping-cart');
+  });
+});
+
